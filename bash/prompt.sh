9a540ebba5a6d49056f971f94f72cd3816cd6162
Nitin Nizhawan <nitin.nizhawan@gmail.com>
2022-08-28 08:08:02 +0530

    Update prompt.sh

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,6 +1,11 @@
 # save this file to /etc/bashrc
+# install powerline-fonts
 
 endColor="\e[m"
-green="\e[0;32m"
-blue="\e[0;34m"
-export PS1="[$blue\u$endColor:$green\W$endColor ]\n\$ "
+greenBack="\e[30;42m"
+green="\e[32m"
+blue="\e[34m"
+blueBack="\e[30;44m"
+endChar=$(echo -e "\ue0b0")
+export PS1="$blueBack$endChar\u$endColor$blue$endChar$endColor$greenBack$endChar\W$endColor$green$endChar$endColor \n\$ "
+
